f9b6e6ff3fe74478a4eb443ace139a8bbdcd1ab4
Mohamed saeed <mohedsh@gmail.com>
2021-06-03 19:01:09 +0200

    update platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "DUTrimVideoView",
+    platforms: [
+           // Add support for all platforms starting from a specific version.
+           .macOS(.v10_15),
+           .iOS(.v11),
+           .watchOS(.v5),
+           .tvOS(.v11)
+       ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
